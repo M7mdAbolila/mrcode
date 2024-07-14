b735d21ddff14a37cfeab7542c01f1ab7e053fca
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-07-14 16:34:15 +0300

    launch mrcode

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mrcode/mrcode_app.dart';
+
+void main() {
+  runApp(const MrcodeApp());
+}
---
+++ lib/mrcode_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MrcodeApp extends StatelessWidget {
+  const MrcodeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('welcome to Mrcode .....'),
+        ),
+      ),
+    );
+  }
+}
